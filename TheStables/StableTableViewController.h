322d33eaa610dc8d3dbc36883fe9183da44f8ad2
T. Andrew Binkowski <abinkowski@uchicago.edu>
2013-03-06 22:52:23 -0600

    Stable Table View Controller

---
+++ TheStables/StableTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  StableTableViewController.h
+//  TheStables
+//
+//  Created by T. Andrew Binkowski on 3/6/13.
+//  Copyright (c) 2013 T. Andrew Binkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StableTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *horses;
+
+@end
